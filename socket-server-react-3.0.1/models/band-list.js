e50492e9ec4af9c1c1fc350ee7aaf690bb3f44e4
Mary <maria.martinez580@misena.edu.co>
2022-09-22 15:54:21 -0500

    one

---
+++ socket-server-react-3.0.1/models/band-list.js
@@ -0,0 +1,48 @@
+const Band = require("./band");
+
+class BandList{
+
+    constructor(){
+
+        this.bands=[
+            new Band('Pandilla'),
+            new Band('Pintura'),
+            new Band('Dibujo'),
+            new Band('Tokyo'),
+        ];
+    }
+    addband(name){
+        const newBand = newBand(name);
+        this.bands.push(newBand);
+        return this.bands
+    }
+    removeBand(id){
+        this.bands = this.bands.filter(band => band.id !== id);
+    }
+
+    increaseVotes(id){
+        this.bands =this.bands.map(band =>{
+
+            if (band.id == id) {
+                band.votes += 1;
+                
+            }
+            return band;
+        })
+    }
+
+    
+    changeName(id,newName){
+        this.bands =this.bands.map(band =>{
+
+            if (band.id == id) {
+                band.name += newName;
+                
+            }
+            return band;
+        })
+    }
+    
+    
+}
+module.exports=BandList
